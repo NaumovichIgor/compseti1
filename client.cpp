7841ffc42cba784db360228a4eb7fc2a16a97b65
NaumovichIgor <igornaumovich123@gmail.com>
2024-12-09 06:43:05 +0300

    Update client.cpp

--- client.cpp
+++ client.cpp
@@ -1,44 +1,52 @@
 #include <iostream>
 #include <fstream>
-#include <string>
 #include <thread>
 #include <chrono>
 
 using namespace std;
 
-void writeRequest(const string& request) {
-    ofstream outFile("f1.txt", ios::app);
+struct Request {
+    char message[256];
+};
+
+struct Response {
+    char message[256];
+};
+
+void writeRequest(const Request& request) {
+    ofstream outFile("f1.bin", ios::binary | ios::app);
     if (outFile.is_open()) {
-        outFile << request << endl;
+        outFile.write(reinterpret_cast<const char*>(&request), sizeof(Request));
         outFile.close();
-    } else {
-        cerr << "Unable to open f1.txt for writing." << endl;
+    }
+    else {
+        cerr << "Unable to open f1.bin for writing." << endl;
     }
 }
 
 void readResponse() {
-    ifstream inFile("f2.txt");
-    string response;
+    Response response;
+    ifstream inFile("f2.bin", ios::binary);
     while (true) {
         if (inFile.is_open()) {
-            while (getline(inFile, response)) {
-                cout << "Response from server: " << response << endl;
+            while (inFile.read(reinterpret_cast<char*>(&response), sizeof(Response))) {
+                cout << "Response from server: " << response.message << endl;
             }
             inFile.close();
         }
-        this_thread::sleep_for(chrono::seconds(1)); // Check every second
-        inFile.open("f2.txt");
+        this_thread::sleep_for(chrono::seconds(1));
+        inFile.open("f2.bin", ios::binary);
     }
 }
 
 int main() {
     thread responseThread(readResponse);
-    string request;
+    Request request;
 
     while (true) {
         cout << "Enter your request (type 'exit' to quit): ";
-        getline(cin, request);
-        if (request == "exit") {
+        cin.getline(request.message, 256);
+        if (string(request.message) == "exit") {
             break;
         }
         writeRequest(request);
